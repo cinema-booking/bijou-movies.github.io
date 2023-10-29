5844d997fca259f207a37ee0889f0c9a1372a7b3
VenomBlac <148686212+VenomBlac@users.noreply.github.com>
2023-10-29 09:21:55 +0000

    Create header.php

---
+++ header.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>Cinema Booking System</title>
+    <link rel="stylesheet" href="../cinema/admin.css">
+  </head>
+  <body> 
+    <!-- Header -->
+    <div class="wrapper header">
+        <ul>
+          <li><a href="http://localhost:3000/cinema/home.php">Bijou Movies</a></li>
+          <li><a href="http://localhost:3000/cinema/book.php">BOOK</a></li>
+          <li><a href="http://localhost:3000/cinema/about.php">ABOUT</a></li>
+          <li><a href="http://localhost:3000/cinema/login.php">Log in</a></li>
+          <li><a href="http://localhost:3000/cinema/signup.php">Sign up</a></li>
+        </ul>
+    </div>
